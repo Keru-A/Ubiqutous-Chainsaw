2aad6c56afbde2ab3f1069d3f811eff4cbc896c3
Alexander Stevenson (1000024146) <STEVAJ3@student.op.ac.nz>
2020-09-24 15:43:35 +1200

    This is going to be great

--- LumberJack/LumberJack/Program.cs
+++ LumberJack/LumberJack/Program.cs
@@ -4,9 +4,57 @@ namespace LumberJack
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int age, dollars = 0, card;
+            string temp;
+            char again;
+            Random rand = new Random();
+
+            Console.Write("Please enter your age:   ");
+            temp = Console.ReadLine();
+            age = Convert.ToInt32(temp);
+
+            if (age < 19)
+            {
+                Console.WriteLine("Good bye you are too young to play.");
+            }
+            else
+            {
+                do
+                {
+                    for (int i = 0; i <= 5; i++)
+                    {
+                        card = rand.Next(1, 14);
+                        switch (card)
+                        {
+                            case 1:
+                                Console.WriteLine("You drew an Ace, collect $20");
+                                dollars = dollars + 20;
+                                Console.WriteLine($"Your current total is {dollars:C}");
+                                break;
+                            case 11:
+                            case 12:
+                            case 13:
+                                Console.WriteLine($"You drew a {card}, collect $10");
+                                dollars = dollars + 10;
+                                Console.WriteLine($"Your current total is {dollars:C}");
+                                break;
+                            default:
+                                Console.WriteLine($"You drew a {card}, you loose {card:C}");
+                                dollars = dollars - card;
+                                Console.WriteLine($"Your current total is {dollars:C}");
+                                break;
+                        }
+                    }
+                    Console.Write("Do you want to play again?   ");
+                    temp = Console.ReadLine();
+                    temp = temp.ToLower();
+                    again = Convert.ToChar(temp[0]);
+                } while (again == 'y');
+            }
+            Console.ReadLine();
+
         }
     }
 }
