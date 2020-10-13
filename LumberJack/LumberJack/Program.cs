6a87aee44287acb5d93908ce2adf27ab0f438daf
Alexander Stevenson (1000024146) <STEVAJ3@student.op.ac.nz>
2020-10-13 14:19:07 +1300

    Added the main medhod

--- LumberJack/LumberJack/Program.cs
+++ LumberJack/LumberJack/Program.cs
@@ -5,9 +5,19 @@ namespace LumberJack
 {
     class Program
     {
+        /* Main method that ask for numbers and the sents them to be sorted*/
         static void Main()
         {
-            Console.WriteLine("The hot water has run out.");
+            double num1, num2, num3;
+            Console.Write("Please enter your first number:   ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please enter your second number:   ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please enter your third number:   ");
+            num3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The current numbers are {num1}, {num2}, and {num3}.");
+            SortNumbers(ref num1, ref num2, ref num3);
+            Console.WriteLine($"The proper order is numbers are {num1}, {num2}, and {num3}.");
         }
     }
 }
