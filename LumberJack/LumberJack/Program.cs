ed54cc02f36de79f2473ae01a9e5b7b9a0571007
Alice Dowle (1000096914) <DOWLAS1@student.op.ac.nz>
2020-10-13 13:59:12 +1300

    remove old code

--- LumberJack/LumberJack/Program.cs
+++ LumberJack/LumberJack/Program.cs
@@ -7,49 +7,7 @@ namespace LumberJack
     {
         static void Main()
         {
-            string temp;
-            int age, round, card = 0, winnings = 0;
-            Random rand = new Random();
-
-            Console.WriteLine("Welcome to the card game. How old are you?");
-            temp = Console.ReadLine();
-            age = Convert.ToInt32(temp);
-
-            if (age >= 18)
-            {
-                for (round = 0; round < 5; round++)
-                {
-                    card = rand.Next(1, 14);
-
-                    switch (card)
-                    {
-                        case 1:
-                            winnings = winnings + 20;
-                            break;
-                        case 11:
-                        case 12:
-                        case 13:
-                            winnings = winnings + 10;
-                            break;
-                        default:
-                            winnings = winnings - card;
-                            break;
-                    }
-
-                    Console.WriteLine($"You drew a {card} card!");
-                    Console.WriteLine($"Your total winnings are {winnings}!");
-                    Thread.Sleep(1000);
-                }
-
-            }
-
-            else
-            {
-                Console.WriteLine("Yo!");
-            }
-
-            Console.WriteLine("Thanks!");
-            Console.ReadLine();
+            
         }
     }
 }
